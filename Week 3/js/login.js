408fd94cbeece8b43fabcb4cd6f99e40b044e9b3
Randy Mackin <heroaurazero@hotmail.com>
2020-02-01 14:27:02 -0800

    week 3 javascripting
    javascripted in docker hub

---
+++ Week 3/js/login.js
@@ -0,0 +1,19 @@
+
+function validate() {
+    
+    document.getElementById('name-error').style.visibility = 'hidden'
+    document.getElementById('password-error').style.visibility = 'hidden'
+
+    let nameField = document.getElementById('name-field').value
+    let passwordField = document.getElementById('password-field').value
+
+    if (nameField == '') {
+        document.getElementById('name-error').style.visibility = 'visible'
+        document.getElementById('name-error').innerHTML = "required"
+    }
+
+    if (passwordField == '') {
+        document.getElementById('password-error').style.visibility = 'visible'
+        document.getElementById('password-error').innerHTML = "required"
+    }
+}
